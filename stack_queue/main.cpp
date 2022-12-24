299816b0078286ff98fb434949582ddb6de56a41
Danu Tryas <Danupristowo09@gmail.com>
2022-12-24 10:08:43 +0700

    first commit + [ADD] stack_queue basic ADT

---
+++ stack_queue/main.cpp
@@ -0,0 +1,40 @@
+#include "stack_queue.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // Simple Program
+    // === Stack
+    // Stack S;
+    // infotype P;
+    // S = createStack();
+    // Push(S, 'A');
+    // Push(S, 'B');
+    // showStack(S);
+    // Pop(S, P);
+    // showStack(S);
+
+    // === Queue
+    // Queue Q;
+    // adrQueue P;
+    // Q = createQueue();
+
+    // P = createElmtQueue('A');
+    // Enqueue(Q, P);
+    // P = createElmtQueue('B');
+    // Enqueue(Q, P);
+    // P = createElmtQueue('C');
+    // Enqueue(Q, P);
+    // showQueue(Q);
+    // Dequeue(Q, P);
+    // showQueue(Q);
+
+    return 0;
+}
+// 1.Insert data (Stack 5, Queue 5)
+// 2.Delete data (Stack 5, Queue 5)
+// 3.Show data (Stack 5, Queue 5)
+// 4.Pemanfaatan primitif dalam penyelesaian masalah, Contoh: infix to prefix dan kasus lainnya. (Stack 30, Queue 30)
+// 5.Main program (10)
